785a729325c870de2f6847b2ea472337a53c9127
LineCoran <vppisya@gmail.com>
2026-01-31 16:54:56 +0300

    .

--- src/common/ui/ui-text.tsx
+++ src/common/ui/ui-text.tsx
@@ -3,17 +3,14 @@ import { ReactNode } from 'react';
 
 const { Paragraph, Title, Text } = Typography;
 
-// Создаем базовый компонент
 export const UiText = ({ children }: { children: ReactNode }) => {
   return <Paragraph>{children}</Paragraph>;
 };
 
-// Присваиваем подкомпоненты
 UiText.Paragraph = Paragraph;
 UiText.Title = Title;
 UiText.Text = Text;
 
-// Опционально: задаем displayName
 UiText.Paragraph.displayName = 'UiText.Paragraph';
 UiText.Title.displayName = 'UiText.Title';
 UiText.Text.displayName = 'UiText.Text';
